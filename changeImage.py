2985585990ea83e64a1a100c9e6883d402b4d112
kevgray83 <75575265+kevgray83@users.noreply.github.com>
2021-11-14 22:03:34 +0000

    Update changeImage.py

--- changeImage.py
+++ changeImage.py
@@ -3,13 +3,13 @@
 from PIL import Image
 import os
 
-image_dir = "~/supplier-data/images"
+image_dir = "supplier-data/images/"
 
 files = os.listdir(image_dir)
 size - (600,400)
 
 for file in files:
-	im = Image.open(file)
+	im = Image.open(image_dir + file)
 	im.convert("RGB")
 	im.resize(size)
 	im.save(file +".jpeg")
